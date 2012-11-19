0a43a6b62e68976681ff37e449a0d2921c20b25c
Jeffrey Chang <thejeff432@gmail.com>
2012-11-18 21:02:33 -0500

    Last commit

--- src/Flavors.java
+++ src/Flavors.java
@@ -1,11 +1,18 @@
 public class Flavors {
-	private static String[] flavors={"Mint Chocolate Chip", "Chocolate", "Vanilla", "Strawberry", "Cookies N' Cream", "Strawberry"};
-	public Flavors(int flavors)
+	private int favorite; //Initial global variables
+	private static String[] flavors={"Mint Chocolate Chip", "Chocolate", "Vanilla", "Strawberry",
+		"Cookies N' Cream", "Strawberry"}; //Array that lists all the flavors
+	private int cost;
+	private boolean nuts;
+	private boolean syrup;
+	private String message = "You bought: ";
+	public Flavors(int flavors) //Constructer that requires instance data
 		{
-		int favorite=flavors;
-		if (flavors>=1 && flavors<=5)
+		favorite=flavors;
+		if (flavors>=1 && flavors<=5) //Loop checks if the query is valid
 		{
-			System.out.println("Your favorite ice cream flavor is " +this.flavors[favorite]);
+			System.out.println("Your favorite ice cream flavor is " +Flavors.flavors[favorite]);
+			cost=3;
 		}
 		else
 			{
@@ -14,17 +21,70 @@ public class Flavors {
 		
 
 		}
-	public static void ListFlavors()
+	public static void ListFlavors()  //Displays a list for the user to choose from
 	{
 	    for (int i=1; i<flavors.length; i++)
 	    {
 	      System.out.println(i+": "+flavors[i]);
 	    } 
 	}
-	public String getFlavors()
+	public String getFlavor() //Getter
 	{
-		return "Your flavor is" +flavors[Flavors.];
+		return flavors[favorite];
 		
 	}
+	public void setFlavor(int newflavor) //Setter
+	{
+		favorite=newflavor;
 	}
-	
+	public void AddNuts(boolean nuts) //Method 1
+	{
+		if (nuts==true)
+		{
+		cost+=1;
+		this.nuts=nuts;
+		}
+	}
+	public void AddSyrup(boolean syrup) //Method 2
+	{
+		if (syrup==true)
+		{
+		cost+=1;
+		this.syrup=syrup;
+		}
+		}
+	public String Receipt(boolean receipt) //Method 3
+	{
+		return "Thank you your cost is $" + cost +".\n Have a good day!";
+	}
+	public void setSyrupAndNuts(boolean nuts, boolean syrup) //Setter for nuts and syrup booleans
+	{
+		this.nuts=nuts;
+		this.syrup=syrup;
+	}
+	public String getSyrupAndNuts() //Getter for nuts and syrup booleans
+	{
+		String nuts_syrup="You have: ";
+		if (nuts==true)
+				nuts_syrup+="Nuts. ";
+		if (syrup==true)
+				nuts_syrup+="Syrup.";
+		else if(nuts==false && syrup==false)
+			nuts_syrup="You have no syrup or nuts";
+		return nuts_syrup;
+		
+	}
+	public String toString()
+	{
+		if (nuts==true)
+		{
+			message += "nuts, ";
+		}
+	    if (syrup==true)
+	    {
+			message += "syrup, ";
+	    }
+	    message+="ice cream.";
+	    return message;
+	}
+	}
--- src/IceCream.java
+++ src/IceCream.java
@@ -8,8 +8,18 @@ public static void main(String[]args)
 	Flavors.ListFlavors();
 	System.out.println("What flavor of ice cream is your favorite?");
 	int flavor=scan.nextInt();
+	scan.close();
 	Flavors flavorobject=new Flavors(flavor);
+	flavorobject.setFlavor(3);
+	flavorobject.getFlavor();
+	flavorobject.AddNuts(true); 
+	flavorobject.AddSyrup(false);
+	flavorobject.setSyrupAndNuts(false, false);
+	System.out.println(flavorobject.getSyrupAndNuts());
+	System.out.println(flavorobject.getFlavor());
 	
+	System.out.println(flavorobject.Receipt(true));
+	System.out.print(flavorobject);
 	
 }}
 
